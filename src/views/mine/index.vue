e1d5e2b2e4060ee6966f518d6149273210ffb9ee
zyjzyj <1321201433@qq.com>
2022-04-16 19:39:21 +0800

    first

---
+++ src/views/mine/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <Header tittle='我的喵喵'></Header>
+      <login/>
+      <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/header'
+import Footer from '@/components/footer'
+import login from '@/components/login'
+export default {
+  name:'mine',
+  components:{
+    Header,
+    Footer,
+    login,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
